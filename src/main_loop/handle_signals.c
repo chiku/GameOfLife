4c2a42e0e319b29491049a2d998b0e98cd5f3903
Chiku <chirantan.mitra@gmail.com>
2012-06-16 11:33:41 +0530

    Explicit signal handling

--- src/main_loop/handle_signals.c
+++ src/main_loop/handle_signals.c
@@ -4,26 +4,25 @@
 
 #include "main_loop.h"
 
-static void handle_signal_for(const char *message, int exit_status)
-{
-	cleanup_on_signal();
-	fprintf((exit_status ? stderr : stdout), message);
-	exit(exit_status);
-}
-
 static void catch_sigint(int sig_num)
 {
-	handle_signal_for("SIGINT received!\n", 0);
+	cleanup_on_signal();
+	fprintf(stdout, "SIGINT received!\n");
+	exit(0);
 }
 
 static void catch_sigterm(int sig_num)
 {
-	handle_signal_for("SIGTERM received!\n", 0);
+	cleanup_on_signal();
+	fprintf(stdout, "SIGTERM received!\n");
+	exit(0);
 }
 
 static void catch_sigkill(int sig_num)
 {
-	handle_signal_for("SIGKILL received!\n", 1);
+	cleanup_on_signal();
+	fprintf(stdout, "SIGKILL received!\n");
+	exit(1);
 }
 
 void initialize_signal_handlers(void)
@@ -32,4 +31,3 @@ void initialize_signal_handlers(void)
 	signal(SIGTERM, catch_sigterm);
 	signal(SIGKILL, catch_sigkill);
 }
-
