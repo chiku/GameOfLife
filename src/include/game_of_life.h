7f739fc11c933b67818452b4b248c1568c634b59
Chiku <chirantan.mitra@gmail.com>
2012-06-30 22:41:35 +0530

    Reduce what game_of_life.h exposes

--- src/include/game_of_life.h
+++ src/include/game_of_life.h
@@ -1,10 +1,27 @@
 #ifndef GOL_GAME_OF_LIFE_H
 #define GOL_GAME_OF_LIFE_H
 
-#include "coordinates.h"
-#include "cell.h"
-#include "world.h"
-#include "rules.h"
-#include "game.h"
+typedef struct Coordinates Coordinates;
+typedef struct Game Game;
+extern struct World World;
+
+struct Coordinates
+{
+	long int x;
+	long int y;
+};
+
+struct Game
+{
+	struct Coordinates *neighbour_locations;
+	struct World *world;
+};
+
+extern void Game_Destroy(Game *self);
+
+extern Game* Game_Create_From_File(const char file_name[]);
+
+extern Game* Game_Tick(Game *self);
+extern void Game_At_Each_Cell(const Game *self, void (*visitor)(struct Coordinates coordinates, void*), void *data);
 
 #endif
