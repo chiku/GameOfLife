d039fbb3fcf3d8e01e0b484ea51f4e631fad8b20
Chiku <chirantan.mitra@gmail.com>
2012-07-29 00:27:24 +0530

    Expose all rules explicitly

--- src/include/rules.h
+++ src/include/rules.h
@@ -2,6 +2,8 @@
 #define GOL_RULE_H
 
 /* Rules Methods */
+int Rule_Carry_Forward_Cell_On_Alive(int neighbours);
+int Rule_Carry_Forward_Cell_On_Dead(int neighbours);
 int Rule_Carry_Forward_Cell(int cell_alive, int neighbours);
 /* Rules Methods */
 
--- src/rules.c
+++ src/rules.c
@@ -1,11 +1,11 @@
 #include "rules.h"
 
-int Rule_Carry_Forward_Alive_Cell(int neighbours)
+int Rule_Carry_Forward_Cell_On_Alive(int neighbours)
 {
 	return neighbours == 2 || neighbours == 3;
 }
 
-int Rule_Carry_Forward_Dead_Cell(int neighbours)
+int Rule_Carry_Forward_Cell_On_Dead(int neighbours)
 {
 	return neighbours == 3;
 }
@@ -13,6 +13,6 @@ int Rule_Carry_Forward_Dead_Cell(int neighbours)
 int Rule_Carry_Forward_Cell(int cell_alive, int neighbours)
 {
 	return cell_alive
-		? Rule_Carry_Forward_Alive_Cell(neighbours)
-		: Rule_Carry_Forward_Dead_Cell(neighbours);
+		? Rule_Carry_Forward_Cell_On_Alive(neighbours)
+		: Rule_Carry_Forward_Cell_On_Dead(neighbours);
 }
