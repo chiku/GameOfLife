3ef39fea1edaf326ec85df22ce419b94d85da1ce
Chiku <chirantan.mitra@gmail.com>
2012-07-29 00:36:57 +0530

    Remove unused method from rules

--- src/include/rules.h
+++ src/include/rules.h
@@ -4,7 +4,6 @@
 /* Rules Methods */
 int Rule_Carry_Forward_Cell_On_Alive(int neighbours);
 int Rule_Carry_Forward_Cell_On_Dead(int neighbours);
-int Rule_Carry_Forward_Cell(int cell_alive, int neighbours);
 /* Rules Methods */
 
 #endif
--- src/rules.c
+++ src/rules.c
@@ -9,10 +9,3 @@ int Rule_Carry_Forward_Cell_On_Dead(int neighbours)
 {
 	return neighbours == 3;
 }
-
-int Rule_Carry_Forward_Cell(int cell_alive, int neighbours)
-{
-	return cell_alive
-		? Rule_Carry_Forward_Cell_On_Alive(neighbours)
-		: Rule_Carry_Forward_Cell_On_Dead(neighbours);
-}
